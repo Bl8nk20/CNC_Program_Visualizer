77ec85706576c50dc82e93fb2364287ffbcfc5cc
Bl8nk20 <moritzschmidt@pop-os.localdomain>
2025-04-03 15:33:11 +0200

    adjusted root CMakeLists.txt

--- cmake/AddExternalLibs.cmake
+++ cmake/AddExternalLibs.cmake
@@ -15,10 +15,13 @@ FetchContent_Declare(
     GIT_SHALLOW TRUE)
 FetchContent_MakeAvailable(spdlog)
 
-# Testing Library
-FetchContent_Declare(
-    Catch2
-    GIT_REPOSITORY https://github.com/catchorg/Catch2
-    GIT_TAG v3.6.0
-    GIT_SHALLOW TRUE)
-FetchContent_MakeAvailable(Catch2)
+if(ENABLE_TESTING)
+    # Testing Library
+    FetchContent_Declare(
+        Catch2
+        GIT_REPOSITORY https://github.com/catchorg/Catch2
+        GIT_TAG v3.6.0
+        GIT_SHALLOW TRUE)
+    FetchContent_MakeAvailable(Catch2)
+    list(APPEND CMAKE_MODULE_PATH ${catch2_SOURCE_DIR}/extras)
+endif()

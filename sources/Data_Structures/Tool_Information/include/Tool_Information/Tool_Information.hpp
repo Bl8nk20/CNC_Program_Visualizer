63c11068e38794d05a717b0d80e46466c2ba6fb5
Bl8nk20 <moritzschmidt@pop-os.localdomain>
2025-04-05 21:55:42 +0200

    added the class variables to the constructors

--- sources/Data_Structures/Tool_Information/include/Tool_Information/Tool_Information.hpp
+++ sources/Data_Structures/Tool_Information/include/Tool_Information/Tool_Information.hpp
@@ -17,14 +17,34 @@ namespace CNC_SIM::DataTypes{
 
         Tool_Information();
 
-        Tool_Information();
+        Tool_Information(const std::string& tool_name, 
+                        const float& diameter, 
+                        const std::optional<int>& amt_cutter,
+                        const std::optional<float>& max_vc,
+                        const std::optional<float>& max_vf,
+                        const std::optional<float>& max_ap);
         
         ~Tool_Information();        
         
         // getter
+        std::string get_ToolName();
+        Tool_Type get_ToolType();
+        float get_Diameter();
+        int get_AmtCutter();
+        float get_MaxVC();
+        float get_MaxVF();
+        float get_MaxAP();
 
         // setter
+        void get_ToolName(const std::string& tool_name);
+        void get_ToolType(const Tool_Type& tool_type);
+        void get_Diameter(const float& diameter);
+        void get_AmtCutter(const int& amt_cutter);
+        void get_MaxVC(const float& max_vc);
+        void get_MaxVF(const float& max_vf);
+        void get_MaxAP(const float& max_ap);
         
+
     private:
         static const std::string tool_name;
         static const Tool_Type Type;
